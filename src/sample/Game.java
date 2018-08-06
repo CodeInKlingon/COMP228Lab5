92a73265a8475215ab74195391e8b2eb990f5e8e
CodeInKlingon <danielphilipstokes@gmail.com>
2018-08-06 12:01:12 -0400

    Initial commit

---
+++ src/sample/Game.java
@@ -0,0 +1,37 @@
+package sample;
+
+public class Game {
+
+    public int ID;
+    String Title;
+
+    //getters and Setters
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+
+    //Constructor
+    public Game(int id, String title){
+        ID = id;
+        Title = title;
+    }
+
+
+    @Override
+    public String toString() {
+        return Title;
+    }
+}
